73b2e2d47af8e783757165328d0409feb0295324
thuytran100401 <thuytran100401@gmail.com>
2021-10-09 03:27:35 -0700

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,16 +1,18 @@
 """ 
 Python program to take in a list as input and multiply all of the 
 elements in the list and return the result as output.
+Parameter
+    inputList: list[]
+                the integer list as input
+    result: int
+                the result of multiplying all of the element
 """
 def multiply_list(inputList):
-    """ 
-    The function multiply_list() will multiply all of the elements 
-        in the list and return the result.
-    """
+
     result = 1; 
     for number in inputList:
         result = result * number
-
+            
     return result
 
 # driver code
