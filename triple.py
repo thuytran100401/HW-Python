92ee1b3b3cdada03690f3561dc7986d93e1e206c
thuytran100401 <thuytran100401@gmail.com>
2021-10-14 21:33:18 -0700

    Update triple.py

--- triple.py
+++ triple.py
@@ -1,3 +1,9 @@
+""" 
+Take a function to use three times
+Parameter
+inner : function
+    function to use func(*agrs) three times
+"""
 def tripler(func):
     def inner(*agrs):
         func(*agrs)
